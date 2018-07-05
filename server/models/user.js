d63495e70d1be34a71bcbaa05d2188f9dbded6b6
Jordan Geltch-Robb <jgeltchrobb@hotmail.com>
2018-07-06 09:46:38 +1000

    added passport to user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,11 +1,17 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Passport-Local Mongoose will add a username, hash and salt field to store the
+// username, the hashed password and the salt value. Additionally Passport-Local
+// Mongoose adds some methods to your Schema.
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const User = new Schema({
-  name: String,
   email: String,
-  password: String,
-  role: String,
-})
+  role: String
+});
 
-module.exports = mongoose.model('User', User)
+// connect passportLocalMongoose and use 'email' instead of 'username'
+User.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', User);
