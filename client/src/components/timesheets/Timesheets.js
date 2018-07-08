e2dfeabc041c8b33a9a99978637b31433ff20a26
Revel Roestorf <revel.roestorf@gmail.com>
2018-07-08 14:29:24 +1000

    bug fixes and commenting out as required - now works

--- client/src/components/timesheets/Timesheets.js
+++ client/src/components/timesheets/Timesheets.js
@@ -72,7 +72,7 @@ class Timesheets extends Component {
             startRostered:  rStart,
             startActual:    aStart,
             start:          this.startTime(rStart, aStart),
-            finishRostered:  rFinsih,
+            finishRostered:  rFinish,
             finishActual:    aFinish,
             finish:         this.finishTime(rFinish, aFinish),
           }
@@ -104,72 +104,70 @@ class Timesheets extends Component {
   render() {
     const { week } = this.props
 
-      if ( !individual ) {
-        return (
-          div>
+    if ( !this.state.individual ) {
+      return (
+        <div>
 
-            <div>
-              <div>  {/* Space left of catagory headings and above names*/}</div>
-              <div>  Ord Hrs           </div>
-              <div>  Sat Hrs           </div>
-              <div>  Annual Leave      </div>
-              <div>  Sick Leave        </div>
-              <div>  Public Hols       </div>
-              <div>  L/S Leave         </div>
-              <div>  Wayne Weekly      </div>
-              <div>  Wayne Sat         </div>
-              <div>  Wayne Sun         </div>
-              <div>  Wayne Public Hols </div>
-              <div>  Sleep Over        </div>
-              <div>  Total             </div>
-            </div>
+          <div>
+            <div>  {/* Space to the left of catagory headings and above names*/}</div>
+            <div>  Ord Hrs           </div>
+            <div>  Sat Hrs           </div>
+            <div>  Annual Leave      </div>
+            <div>  Sick Leave        </div>
+            <div>  Public Hols       </div>
+            <div>  L/S Leave         </div>
+            <div>  Wayne Weekly      </div>
+            <div>  Wayne Sat         </div>
+            <div>  Wayne Sun         </div>
+            <div>  Wayne Public Hols </div>
+            <div>  Sleep Over        </div>
+            <div>  Total             </div>
+          </div>
 
 
-            <div>
-              <Summary  staffNames={this.state.staffNames}
+          <div>
+            <Summary  staffNames={this.state.staffNames}
+                      timesheetData={this.state.timesheetData}
+                      setIndividual={this.setIndividual}
+            />
+          </div>
+
+        </div>
+      )
+
+    } else {
+      return (
+        <div>
+
+          <div>
+            <div>  {/* Space left of catagory headings and above names*/}</div>
+            <div>  Ord Hrs           </div>
+            <div>  Sat Hrs           </div>
+            <div>  Annual Leave      </div>
+            <div>  Sick Leave        </div>
+            <div>  Public Hols       </div>
+            <div>  L/S Leave         </div>
+            <div>  Wayne Weekly      </div>
+            <div>  Wayne Sat         </div>
+            <div>  Wayne Sun         </div>
+            <div>  Wayne Public Hols </div>
+            <div>  Sleep Over        </div>
+            <div>  Total             </div>
+          </div>
+
+
+          <div>
+            <Individual staffNames={this.state.staffNames}
                         timesheetData={this.state.timesheetData}
+                        individual={this.state.individual}
                         setIndividual={this.setIndividual}
-              />
-            </div>
-
+                        removeIndividual={this.removeIndividual}
+            />
           </div>
-        )
 
-      } else {
-        return (
-          div>
-
-            <div>
-              <div>  {/* Space left of catagory headings and above names*/}</div>
-              <div>  Ord Hrs           </div>
-              <div>  Sat Hrs           </div>
-              <div>  Annual Leave      </div>
-              <div>  Sick Leave        </div>
-              <div>  Public Hols       </div>
-              <div>  L/S Leave         </div>
-              <div>  Wayne Weekly      </div>
-              <div>  Wayne Sat         </div>
-              <div>  Wayne Sun         </div>
-              <div>  Wayne Public Hols </div>
-              <div>  Sleep Over        </div>
-              <div>  Total             </div>
-            </div>
-
-
-            <div>
-              <Individual staffNames={this.state.staffNames}
-                          timesheetData={this.state.timesheetData}
-                          individual=(this.state.individual)
-                          setIndividual={this.setIndividual}
-                          removeIndividual={this.removeIndividual}
-              />
-            </div>
-
-          </div>
-        )
-      }
+        </div>
+      )
     }
-
   }
 
 }
--- client/src/components/timesheets/summary/Summary.js
+++ client/src/components/timesheets/summary/Summary.js
@@ -9,67 +9,70 @@ class Summary extends Component {
 
   }
 
-  componentDidMount = () => {
-
-    const { timesheetData } = this.props
-    // [ { staffID:'', shifts:[ { date:'', catagory:'',
-    //                            startRostered:'', startActual:'', start:'',
-    //                            finishRostered:'', finishActual:'', finish:''
-    // }]}]
-
-    const totalsRows = []
-    // [{ staffID: '', floor: '', room: '', kitchen: '', cat4: '', cat5: '',
-    //      cat6: '', cat7: '', cat8: '', cat9: '', cat10: '', cat11: ''} }, {},{}]
-
-    timesheetData.map((staffMember) => {
-      const totalsRow = {}
-      totalsRow.staffID = staffMember.staffID
-      // here need to loop through the official category list and poplate into
-      // totalsRow as keys set to empty strings
-      // Fuck.. what are the categories and how do they translate to hours categories?
-      staffMember.shifts.map((shift) => {
-        if (!totalsRow[shift.category]) {
-          totalsRow[shift.category] = (shift.finish - shift.start)
-        } else {
-          totalsRow[shift.category] += (shift.finish - shift.start)
-        }
-      })
-      totalsRows.push(totalsRow)
-    })
-    this.setState({
-      totalsRows: totalsRows,
-    })
-
-  }
+  // componentDidMount = () => {
+  //
+  //   const { timesheetData } = this.props
+  //   // [ { staffID:'', shifts:[ { date:'', catagory:'',
+  //   //                            startRostered:'', startActual:'', start:'',
+  //   //                            finishRostered:'', finishActual:'', finish:''
+  //   // }]}]
+  //
+  //   const totalsRows = []
+  //   // [{ staffID: '', floor: '', room: '', kitchen: '', cat4: '', cat5: '',
+  //   //      cat6: '', cat7: '', cat8: '', cat9: '', cat10: '', cat11: ''} }, {},{}]
+  //
+  //   timesheetData.map((staffMember) => {
+  //     const totalsRow = {}
+  //     totalsRow.staffID = staffMember.staffID
+  //     // here need to loop through the official category list and poplate into
+  //     // totalsRow as keys set to empty strings
+  //     // Fuck.. what are the categories and how do they translate to hours categories?
+  //     staffMember.shifts.map((shift) => {
+  //       if (!totalsRow[shift.category]) {
+  //         totalsRow[shift.category] = (shift.finish - shift.start)
+  //       } else {
+  //         totalsRow[shift.category] += (shift.finish - shift.start)
+  //       }
+  //     })
+  //     totalsRows.push(totalsRow)
+  //   })
+  //   this.setState({
+  //     totalsRows: totalsRows,
+  //   })
+  //
+  // }
 
   nameToTop = () => {
     {/* Take name/id array and sort clicked name to front */}
   }
 
   render() {
-    const { staffNames, etIndividual } = this.props
+    const { staffNames, setIndividual } = this.props
 
     return (
       <div>
 
         <div className='names-constainer'>
 
-          staffNames.map((staffID) => {
+{/*          staffNames.map((staffID) => {
             return (
-              <Name staffID={staffID}/>
+              <Name staffID={staffID} />
             )
           })
+*/}           
         </div>
-
+{/*
         <div>
           totalsRows.map((row) => {
             <TotalsRow />
           })
         </div>
+*/}
 
       </div>
     )
   }
+
 }
 
 export default Summary
