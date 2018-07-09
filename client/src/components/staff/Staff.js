13885a644d821d477844931ac365a9aacf065c38
Jordan Geltch-Robb <jgeltchrobb@hotmail.com>
2018-07-09 15:09:20 +1000

    initial staff page commit

---
+++ client/src/components/staff/Staff.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class Staff extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default Staff
