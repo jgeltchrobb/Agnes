6d6cea16ac786940076da379e35c06fa78a4f8d3
Jordan Geltch-Robb <jgeltchrobb@hotmail.com>
2018-07-12 21:57:11 +1000

    rost cells fixed

--- client/src/components/staff/cells/RostCells.js
+++ client/src/components/staff/cells/RostCells.js
@@ -1,26 +1,11 @@
 import React from 'react'
 
 const RostCells = (props) => {
-  const catArr = [{Ordinary: 'Ordinary'},
-        {Sat: 'Sat'},
-        {Sun: 'Sun'},
-        {night: 'Night'}]
-
-  for (let key of Object.keys(props)) {
-    for (let obj of catArr) {
-      console.log(obj[key])
-      if (key === obj) {
-        return (
-          <div className="cell" >
-            <h3>{obj[key]}</h3>
-            {/* <h4>{props.rostered}</h4> */}
-          </div>
-      )
-      } else {
-        return ''
-      }
-    }
-  }
+  return (
+    <div className="cell" >
+      <h4>{props.rostered}</h4>
+    </div>
+  )
 }
 
 export default RostCells
