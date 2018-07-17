b0af971bcad564b8c2257d4c12a29fd7f9cf233c
Revel <revel.roestorf@gmail.com>
2018-07-17 16:53:28 +1000

    full branch of previous

--- server/routes/weeks.js
+++ server/routes/weeks.js
@@ -103,38 +103,29 @@ router.delete('/:id', async (req, res) => {
 })
 
 router.post('/shift/:id', async (req, res) => {
-  console.log(req.body)
-  console.log('!!!!!!!!!!!')
-  console.log(req.params)
-
-  let week = await Week.findOne({ _id: req.body.weekID })
-  console.log(week, 'week')
-  try {
-    let found = false
-<<<<<<< HEAD
-=======
-    let week = await Week.findOne({_id: req.body.weekID})
-    console.log(week)
->>>>>>> 4990eb1779ec41734b070cb3e900cd8a956f38b4
-    for (let staff of week.staff) {
-      if (staff.staffID === req.params.id) {
-        for (let shift of staff.shifts) {
-          if (shift.date === req.body.shift.date && shift.start.rostered === req.body.shift.start.rostered) {
-            shift = req.body.shift
-            week.save()
-            found = true
-          }
-        }
-        if (!found) {
-          staff.shifts.push(req.body.shift)
-          week.save()
-        }
-      }
-    }
-    res.send(week)
-  } catch (error) {
-    res.status(500).json({ error: error.message })
-  }
+ try {
+   let found = false
+   let week = await Week.findOne({_id: req.body.shiftObj.weekID})
+   for (let staff of week.staff) {
+     if (staff.staffID === req.params.id) {
+       for (let shift of staff.shifts) {
+         if (shift.date === req.body.shiftObj.shift.date) {
+           shift = req.body.shiftObj.shift
+           week.save()
+           found = true
+         }
+       }
+       if (!found) {
+         staff.shifts.push(req.body.shiftObj.shift)
+         week.save()
+       }
+     }
+   }
+   // && shift.start.rostered === req.body.shift.start.rostered
+   res.send(week)
+ } catch (error) {
+   res.status(500).json({ error: error.message })
+ }
 })
 
 module.exports = router
